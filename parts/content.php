87fa4b46eb1fd935c0d76fdd3cbde04cfbb177ae
dualjack <jakubkuranda@gmail.com>
2018-05-17 00:27:11 +0200

    Fix: Post class error.

--- parts/content.php
+++ parts/content.php
@@ -2,7 +2,7 @@
 
 echo apply_filters( 'front_post_before', '' );
 
-printf( '<div %1$s>', get_post_class() );
+printf( '<div class="%1$s">', implode( ' ', get_post_class() ) );
 
 echo apply_filters( 'front_post_title', sprintf( '<h1>%1$s</h1>', get_the_title() ) );
 echo apply_filters( 'front_post_content', sprintf( '<article>%1$s</article>', get_the_content() ) );
